1a6a67c6349c11e2aca62c105339b125757dce84
Hillary Tao <hillarytao@Hillarys-MacBook-Pro.local>
2020-09-25 17:37:54 -0400

    first version done

--- ahp.py
+++ ahp.py
@@ -11,25 +11,27 @@ import operator
 n = 4
 
 # Creating default matrix of ones
-#A = np.ones([n,n])
+A = np.ones([n,n])
 # Running a for loop to take answer from user and populate the upper triangular elements
-#for i in range(0,n):
-    #for j in range(0,n):
-         #if i<j:
-             #aij = input('How important is option{} over option{} ?: '.format(i,j)) #this to change for later
+for i in range(0,n):
+    for j in range(0,n):
+         if i<j:
+             aij = input('How important is option{} over option{} ?: '.format(i,j)) #this to change for later
              #because depends on nature of raw data
-             #A[i,j] = float(aij) #Upper triangular elements
-             #A[j,i] = 1/float(aij) #Lower triangular elements
+             A[i,j] = float(aij) #Upper triangular elements
+             A[j,i] = 1/float(aij) #Lower triangular elements
 
 
 #compute the priority vector by normalizing the eigen vector of the largest eigen value.
-#The elements in this eigen vector are the weights of the criteria.
-#eig_val = np.linalg.eig(A)[0].max()
-#e = np.linalg.eig(A)[1][:,0]
-#p = e/e.sum()
-
+#he elements in this eigen vector are the weights of the criteria.
+eig_val = np.linalg.eig(A)[0].max()
+e = np.linalg.eig(A)[1][:,0]
+p = e/e.sum()
+ #just to see the criterias against each other
+print(A)
 
 #this is to combine the above together such that it is more cohesive together-more reliable
+#also starting here, this is for the 4 different 
 def pairwise_matrix(n):
     """
     #call this function for generating pair-wise comparison matrices
@@ -47,24 +49,28 @@ def pairwise_matrix(n):
                 aij = input('How important is option{} over option{} ?: '.format(i,j)) #to channge according to the approriate collection format of raw data 
                 A[i,j] = float(aij)
                 A[j,i] = 1/float(aij) #this part here for priority
-    print(A)            
-    #Computing the priority vector (basically the weight) 
+                
+    #Computing the priority vector (basically the weight)-for basically each category 
     eig_val = np.linalg.eig(A)[0].max()
     eig_vec = np.linalg.eig(A)[1][:,0]
     p = eig_vec/eig_vec.sum()
     return p, eig_val
 
-#labeling the different criterias
+#labeling the different criterias, where it measures out importance of {SIPS,IPS, NIPT, and no test} which is the alternative set
+#just to be able to access more easily
+print("\n")
 pr_c = pairwise_matrix(4)[0]  #All Criteria
+print("\n")
 pr_c0 = pairwise_matrix(4)[0] #Criteria 0: Waiting time
+print("\n")
 pr_c1 = pairwise_matrix(4)[0] #Criteria 1: Cost
+print("\n")
 pr_c2 = pairwise_matrix(4)[0] #Criteria 2: Detection rate
-pr_c2 = pairwise_matrix(4)[0] #Criteria 3: Week of pregnancy that test will be taken
+print("\n")
+pr_c3 = pairwise_matrix(4)[0] #Criteria 3: Week of pregnancy that test will be taken
+print("\n")
+# final step is to get the weighted arithmetic sum to yield the rank vector
+r = pr_c0*pr_c[0] + pr_c1*pr_c[1] + pr_c2*pr_c[2]+pr_c3*pr_c[3]
+print(r)
 
-r = pr_c0*pr_c[0] + pr_c1*pr_c[1] + pr_c2*pr_c[2]
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
-    pairwise_matrix(4)
-    print(r)
